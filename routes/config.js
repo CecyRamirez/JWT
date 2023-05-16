a383678fc3a2e779d44e65a756adea104a159291
Cecilia <cramirez25@ucol.mx>
2023-05-16 08:43:03 -0600

    dev env

--- routes/config.js
+++ routes/config.js
@@ -1,9 +1,16 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+});
 
 module.exports = {
 
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || '127.0.0.1',
-  PORT: process.env.PORT || 3000
-
+  PORT: process.env.PORT || 3000,
+  USERNAME: 'root',
+  PASS: 'MonstaX15',
+  DB: process.env.db
 }
